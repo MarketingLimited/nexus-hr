4ba4e40043fc05a239da25f50770c970f3c281de
Mohamed Jasim Ahmed <30666269+MarketingLimited@users.noreply.github.com>
2025-07-31 15:12:29 +0300

    Add mock API endpoints and test setup

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom'
 import { cleanup } from '@testing-library/react'
 import { afterEach, beforeAll, afterAll } from 'vitest'
 import { server } from './test-utils/msw-server'
+import { setupTestEnvironment } from './test-utils'
 import { expect } from 'vitest'
 import { toHaveNoViolations } from 'jest-axe'
 
@@ -14,11 +15,18 @@ afterEach(() => {
 })
 
 // Establish API mocking before all tests.
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+let cleanupEnv: () => void
+beforeAll(() => {
+  cleanupEnv = setupTestEnvironment()
+  server.listen({ onUnhandledRequest: 'error' })
+})
 
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
 afterEach(() => server.resetHandlers())
 
 // Clean up after the tests are finished.
-afterAll(() => server.close())
+afterAll(() => {
+  cleanupEnv && cleanupEnv()
+  server.close()
+})
